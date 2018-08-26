ad8ff155c9c0bb13bf5b71329c07d5efe19eb436
Andrey Yurtaev <mdrdnt@gmail.com>
2018-08-26 16:35:37 -0400

    Added disabling online links when offline.

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
+
+window.onload = function() {
+  if (!navigator.onLine) {
+    document.body.classList.add("offline");
+  }
+};
+
+window.addEventListener("load", () => {
+  function handleNetworkChange(event) {
+    if (navigator.onLine) {
+      document.body.classList.remove("offline");
+    } else {
+      document.body.classList.add("offline");
+    }
+  }
+  window.addEventListener("online", handleNetworkChange);
+  window.addEventListener("offline", handleNetworkChange);
+});
