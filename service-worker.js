576fd336587a6c8f12732cbf76d25b981a6f84f3
Andrey Yurtaev <mdrdnt@gmail.com>
2019-06-24 19:56:16 +0000

    Updated SPB and MNK maps.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var dataCacheName = 'mm2-pwa-v1.2';
-var cacheName = 'mm2-pwa-v1.2';
+var dataCacheName = 'mm2-pwa-v1.3';
+var cacheName = 'mm2-pwa-v1.3';
 var filesToCache = [
  "./fonts/MaterialIcons-Regular.eot",
  "./fonts/MaterialIcons-Regular.ttf",
