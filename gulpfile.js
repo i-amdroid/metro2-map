c29cec08e43bf160d4af84ce6cd37c15f42e0a54
Andrey Yurtaev <mdrdnt@gmail.com>
2024-02-05 19:04:37 +0000

    Updated npm dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
